4a82578620a26328f24e8f169a7c8afb27da7a90
Clément Chanchevrier <67628843+cchanche@users.noreply.github.com>
2022-04-04 23:17:55 +0200

    Add opacity slider
    Added slider to control opacity of the overlay. 
    
    Credits to github.com/onyx-4977/onyx-4977

--- kcorp.user.js
+++ kcorp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Place - Armée de Kameto
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.1
 // @description  On va récuperer ce qui nous est du de droit.
 // @author       Adcoss95
 // @match        https://hot-potato.reddit.com/embed*
@@ -15,19 +15,31 @@ if (window.top !== window.self) {
             const i = document.createElement("img");
             i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            i.className = "overlay_template"
             console.log(i);
-            document.addEventListener("keydown", function(event) {
-                if(event.key == "F4"){
-                    if (i.style.display === "none") {
-                        i.style.display = "block";
-                    } else {
-                        i.style.display = "none";
-                    }
-                }
-            });
             return i;
-        })())
- 
+        })());
+
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByClassName("bottom-controls")[0].appendChild(
+        (function () {
+            const slider = document.createElement("div");
+            slider.style = "height: 36px; width: 200px; position: absolute;  right: 100px; top: 0;  background-color: #FFF;pointer-events: all;border-radius: 26px;";
+            const input = document.createElement("input");
+            input.type = 'range';
+            input.min = '0';
+            input.max = '1';
+            input.step = '0.1';
+            input.value = '1';
+            input.style = "margin: 10px;left: 0;right: 0;top: 0;bottom: 0;box-sizing: border-box;position: absolute;";
+            input.id = 'templateSlider'
+            input.addEventListener('input', (event) => {
+                document.querySelector("body > mona-lisa-app > faceplate-csrf-provider > faceplate-alert-reporter > mona-lisa-embed").shadowRoot.querySelector("div > mona-lisa-share-container > mona-lisa-camera > mona-lisa-canvas").shadowRoot.querySelector("div > img").style.opacity = event.currentTarget.value
+            });
+            slider.appendChild(input);
+            return slider;
+        })()
+        );
+
     }, false);
- 
+
 }
