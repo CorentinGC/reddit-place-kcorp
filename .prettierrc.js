46fed27edab0692b94021deb203f1ba86ea44ff3
Tran Tâm-Tanguy <thanh-tam-tangu.tran@epita.fr>
2022-04-04 22:38:50 +0200

    feat: Add auto-update + refactor

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  printWidth: 90,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  bracketSpacing: false,
+}
--- kcorp.user.js
+++ kcorp.user.js
@@ -2,32 +2,73 @@
 // @name         Reddit Place - Armée de Kameto
 // @namespace    http://tampermonkey.net/
 // @version      0.4
+// @updateURL    https://github.com/CorentinGC/reddit-place-kcorp/raw/main/kcorp.user.js
+// @downloadURL  https://github.com/CorentinGC/reddit-place-kcorp/raw/main/kcorp.user.js
 // @description  On va récuperer ce qui nous est du de droit.
 // @author       Adcoss95
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
 // @grant        none
 // ==/UserScript==
-if (window.top !== window.self) {
-    window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-            console.log(i);
-            document.addEventListener("keydown", function(event) {
-                if(event.key == "F4"){
-                    if (i.style.display === "none") {
-                        i.style.display = "block";
-                    } else {
-                        i.style.display = "none";
-                    }
-                }
-            });
-            return i;
-        })())
- 
-    }, false);
- 
+
+/**
+ * @param {HTMLImageElement} img
+ */
+function applyImgStyles(img) {
+  img.style.position = "absolute"
+  img.style.left = "0"
+  img.style.top = "0"
+  img.style.imageRendering = "pixelated"
+  img.style.width = "2000px"
+  img.style.height = "2000px"
 }
+
+/**
+ * @returns {HTMLImageElement}
+ */
+function createOverlay() {
+  // Create img node.
+  const img = document.createElement("img")
+
+  img.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png"
+  applyImgStyles(img)
+
+  return img
+}
+
+/**
+ * @returns {HTMLImageElement}
+ */
+function appendImg() {
+  const canvas = document
+    .getElementsByTagName("mona-lisa-embed")[0]
+    .shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot
+    .children[0]
+
+  const overlay = createOverlay()
+  canvas.appendChild(overlay)
+
+  return overlay
+}
+
+function onLoad() {
+  const overlay = appendImg()
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key == "F4") {
+      if (overlay.style.display === "none") {
+        overlay.style.display = "block"
+      } else {
+        overlay.style.display = "none"
+      }
+    }
+  })
+}
+
+function main() {
+  if (window.top === window.self) return
+
+  window.addEventListener("load", onLoad)
+}
+
+main()
