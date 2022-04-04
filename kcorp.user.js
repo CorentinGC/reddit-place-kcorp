b40cab508f8d50d2ab13c2fad8852590ec9478b8
Matthieu Renault <matthieu.renault@ob-do.com>
2022-04-04 20:09:57 +0200

    Reload shortcut (cache busted URL) & overlay toggle

--- kcorp.user.js
+++ kcorp.user.js
@@ -8,17 +8,41 @@
 // @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
 // @grant        none
 // ==/UserScript==
+
+function overlayURL() {
+
+    //Cache busted URL
+    return "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png?" + (new Date().getTime());
+}
+
 if (window.top !== window.self) {
+
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-            console.log(i);
-            return i;
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild((function() {
+
+            const overlay= document.createElement("img");
+            overlay.src = overlayURL();
+            overlay.style = "display: block; position: absolute; left: 0; top: 0; image-rendering: pixelated; width: 2000px; height: 2000px;";
+
+            window.addEventListener("keydown", function (event) {
+            
+                // Hide the image with the F4 key
+                if (event.key == "F4") {
+
+                    // Toggle overlay
+                    var display = (overlay.style.display == "none" ? "block" : "none");
+
+                    overlay.style.display = display;
+
+                    //Reload overlay
+                    if (display == "block") {
+                        overlay.src = overlayURL()
+                    }
+                }
+            });
+
+            return overlay;
         })())
  
     }, false);
- 
 }
