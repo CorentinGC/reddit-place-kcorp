a420a3bbadbf1befbf91f79abcd5a6b088cd5498
jvincent <janisvincent@gmail.com>
2022-04-05 00:31:45 +0200

    feat: ensure grid display, refresh layer

--- kcorp.user.js
+++ kcorp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Place - Armée de Kameto
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  On va récuperer ce qui nous est du de droit.
 // @author       Adcoss95
 // @match        https://hot-potato.reddit.com/embed*
@@ -9,25 +9,44 @@
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
+    const overlayURL = () => {
+        return "https://raw.githubusercontent.com/CorentinGC/reddit-place-kcorp/main/overlay.png?time=" + (new Date().getTime());
+    };
+
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
+        const t = setInterval(() => {
+            let e = document.getElementsByTagName("mona-lisa-embed");
+            if ("undefined" === typeof e || e.length < 1) {
+                return;
+            }
+
+            let c = e[0].shadowRoot.children[0].getElementsByTagName('mona-lisa-canvas');
+            if ("undefined" === typeof c || c.length < 1) {
+                return;
+            }
+
+            let cc = c[0].shadowRoot.children[0].getElementsByTagName('canvas');
+            if ("undefined" === typeof cc || cc.length < 1) {
+                return;
+            }
+
             const i = document.createElement("img");
-            i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
+            i.src = overlayURL();
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-            console.log(i);
-            document.addEventListener("keydown", function(event) {
-                if(event.key == "F4"){
+
+            cc[0].parentNode.appendChild(i);
+            clearInterval(t);
+
+            document.addEventListener("keydown", (event) => {
+                if (event.key === "F4") {
                     if (i.style.display === "none") {
+                        i.src = overlayURL();
                         i.style.display = "block";
                     } else {
                         i.style.display = "none";
                     }
                 }
             });
-            return i;
-        })())
- 
+        }, 3000);
     }, false);
- 
 }
